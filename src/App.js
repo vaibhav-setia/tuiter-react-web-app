4ddea2bb223d3215680ca19b3a8ca7924f64ce57
Vaibhav Setia <vaibhavwimpsta@gmail.com>
2023-05-28 18:05:15 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ function App() {
     <BrowserRouter>
       <div className="container">
         <Routes>
-         /* <Route path="/labs"
-                 element={<Labs/>}/> */}
-                    <Route index
-                 element={<Labs/>}/>
+         <Route path="/labs/*"   element={<Labs/>}/>
+
           <Route path="/hello" element={<HelloWorld />} />
           <Route path="/tuiter/*" element={<Tuiter />} />
         </Routes>
