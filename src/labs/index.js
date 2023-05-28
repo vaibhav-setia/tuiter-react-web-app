b34c4f1a11c673caf294f3c94065222806d0a00c
Vaibhav Setia <vaibhavwimpsta@gmail.com>
2023-05-28 17:58:12 -0400

    Update index.js

--- src/labs/index.js
+++ src/labs/index.js
@@ -2,12 +2,16 @@ import Assignment3 from "./a3";
 import JavaScript from "./a3/java-script";
 import { Link } from "react-router-dom";
 import Nav from "../nav";
+import {Routes, Route}
+  from "react-router";
 function Labs() {
   return (
     <div>
       <Nav />
-
-      <Assignment3 />
+       <Routes>
+       <Route path="a3"
+        element={<Assignment3/>}/>
+ </Routes>
     </div>
   );
 }
