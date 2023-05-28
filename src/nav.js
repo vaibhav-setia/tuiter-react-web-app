93d0ce7b5611aab079b138612daeabc534160bd6
Vaibhav Setia <vaibhavwimpsta@gmail.com>
2023-05-28 18:01:15 -0400

    Update nav.js

--- src/nav.js
+++ src/nav.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <nav className="nav nav-tabs mb-2">
-      <Link className="nav-link" to="/labs/a3">
+      <Link className="nav-link" to="/labs/*">
         A3
       </Link>
       <Link className="nav-link" to="/hello">
