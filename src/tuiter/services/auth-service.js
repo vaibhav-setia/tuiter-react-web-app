6d1a708642d0635e24d7c990a0cad7efbe420484
vsetia <setia.v@northeastern.edu>
2023-06-12 18:28:53 -0400

    assig 5

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
-const USERS_URL = `${SERVER_API_URL}/users`;
+const SERVER_API_URL = process.env.REACT_APP_API_BASE;
+const USERS_URL = `${SERVER_API_URL}/api/users`;
 
 const api = axios.create({ withCredentials: true });
 
