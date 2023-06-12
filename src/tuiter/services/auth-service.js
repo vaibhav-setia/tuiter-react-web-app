a739b95519b47ee528cec1eb2af4ee4621d06e04
vsetia <setia.v@northeastern.edu>
2023-06-12 19:46:51 -0400

    fixing update code

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -19,7 +19,7 @@ export const profile = async () => {
   return response.data;
 };
 export const updateUser = async (user) => {
-  const response = await api.put(`${USERS_URL}/${user._id}`, user);
+  const response = await api.put(`${USERS_URL}`, user);
   return response.data;
 };
 export const register = async ({ username, password }) => {
